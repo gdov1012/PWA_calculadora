ad7e073f9d02413363d5723f2840aca280c3256b
gdov1012 <valerogiovanna7@gmail.com>
2025-09-03 20:37:16 -0300

    atualização cache

--- sw.js
+++ sw.js
@@ -1,14 +1,28 @@
+const CACHE_NAME = "calculadora/v1";
+const LISTA = [
+   "/PWA_calculadora/icons/256.png",
+   "/PWA_calculadora/icons/512.png",
+   "/PWA_calculadora/manifest.json",
+   "/PWA_calculadora/style.css",
+   "/PWA_calculadora/sw.js",
+   "/PWA_calculadora/index.html",
+   "/PWA_calculadora/calculadora.js"
+]
+
+ 
 
 self.addEventListener('install', event => {
     console.log('Service Worker instalado');
+    caches.open(CACHE_NAME).then((cache) => {
+        cache.addAll(LISTA)
+     })
 });
  
-self.addEventListener('activate', event => {
-    console.log('Service Worker ativado');
-});
- 
+
 self.addEventListener('fetch', event => {
     event.respondWith(
-        fetch(event.request).catch(() => new Response("Você está offline"))
+       caches.match(event.request).then((response)=>{
+        return response ||  fetch(event.request).catch(() => new Response("Você está offline"))
+       })
     );
 });
